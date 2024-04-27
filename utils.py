6a73e8e7f1f537c44a13cb6404a6ece8fdf77872
hamzaathar <74014297+hamzaathar@users.noreply.github.com>
2024-04-27 12:45:45 +0400

    head

---
+++ utils.py
@@ -0,0 +1,45 @@
+from torch import nn
+
+class Head(nn.Module):
+    def __init__(
+        self,
+        in_dim,
+        out_dim,
+        hidden_dim=512,
+        bottleneck_dim=256,
+        n_layers=3,
+        norm_last_layer=False,
+    ):
+        super().__init__()
+        if n_layers == 1:
+            self.mlp = nn.Linear(in_dim, bottleneck_dim)
+        else:
+            layers = [nn.Linear(in_dim, hidden_dim)]
+            layers.append(nn.GELU())
+            for _ in range(n_layers - 2):
+                layers.append(nn.Linear(hidden_dim, hidden_dim))
+                layers.append(nn.GELU())
+            layers.append(nn.Linear(hidden_dim, bottleneck_dim))
+            self.mlp = nn.Sequential(*layers)
+
+        self.apply(self._init_parameters)
+
+        self.last_layer = nn.utils.weight_norm(
+            nn.Linear(bottleneck_dim, out_dim, bias=False)
+        )
+        self.last_layer.weight_g.data.fill_(1)
+        if norm_last_layer:
+            self.last_layer.weight_g.requires_grad = False
+
+    def _init_parameters(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, std=0.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.mlp(x)  # (n_samples, bottleneck_dim)
+        x = nn.functional.normalize(x, dim=-1, p=2)  # (n_samples, bottleneck_dim)
+        x = self.last_layer(x)  # (n_samples, out_dim)
+
+        return x
